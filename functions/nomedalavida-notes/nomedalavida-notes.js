bb39d591866c0d9efa53dd5af4ad65f75fc4e1df
Dawntraoz <albasilventefu@gmail.com>
2023-06-19 18:23:06 +0200

    debug: trying consoles to see where are being rendered

--- functions/nomedalavida-notes/nomedalavida-notes.js
+++ functions/nomedalavida-notes/nomedalavida-notes.js
@@ -9,10 +9,12 @@ exports.handler = async function (event, context) {
       block_id: process.env.NOTION_NMDLV_ID,
       page_size: 50,
     })).results;
+    console.log(notesProdPage);
   } catch (error) {
     console.error(error);
   }
   const podcastSeasons = notesProdPage.filter(block => block.callout);
+  console.log(podcastSeasons);
 
   let episodeNotes = {};
   podcastSeasons.map(async version => {
@@ -21,10 +23,12 @@ exports.handler = async function (event, context) {
         block_id: version.id,
         page_size: 50,
       });
+      console.log(episodeNotesByVersion);
       episodeNotes[version.callout.rich_text] = episodeNotesByVersion.results.map(episode => ({
         notion_id: episode.id,
         title: episode.child_page.title
       }));
+      console.log(episodeNotes);
     } catch (error) {
       console.error(error);
     }
