d9568fe286ec594bd9d855dd021801de9508a0c3
Dawntraoz <albasilventefu@gmail.com>
2023-06-19 17:15:12 +0200

    feat: first commit

---
+++ functions/notion/notion.js
@@ -0,0 +1,24 @@
+const { Client } = require("@notionhq/client")
+
+exports.handler = async function (event, context) {
+  const notionClient = new Client({ auth: process.env.NOTION_KEY });
+  const database = await notionClient.databases.query({
+    database_id: process.env.NOTION_PAGE_ID,
+  });
+  const posts = [];
+
+  database.results.map(item => {
+    posts.push({
+      id: item.id,
+      title: item.properties.Title.title[0].text.content,
+      publicationDate: item.properties["Publication date"].date.start,
+      link: item.properties.Link.url,
+      tags: item.properties.Tags.multi_select.map(tag => tag.name),
+    });
+  });
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(posts)
+  }
+}
